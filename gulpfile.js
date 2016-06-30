5977749f0ff2a9c97e4ab7b83781569c64d478cc
burkanov <jurij@burkanov.com>
2016-06-30 14:52:51 +0200

    finalized gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,99 @@
 // Reqs
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
 
-// Tasks
-gulp.task('useref', function(){
-  return gulp.src('app/**/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'))
-});
+// Development Tasks
+// -----------------
 
-gulp.task('sass', function() {
-  return gulp.src('app/sass/*.sass')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
-
+// Start browserSync server
 gulp.task('browserSync', function() {
-  browserSync.init({
+  browserSync({
     server: {
       baseDir: 'dist'
-    },
-  });
+    }
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/sass/*.sass') // Gets all files ending with .sass in app/sass and children dirs
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Passes it through a gulp-sass
+    .pipe(gulp.dest('dist/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('app/sass/*.sass', ['sass']);
+  gulp.watch('app/**/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 });
 
-gulp.task('watch', ['browserSync', 'sass', 'useref'], function(){
-  gulp.watch('app/sass/*.sass', ['sass']);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
-  gulp.watch('app/**/*.html', browserSync.reload);
+// Optimization Tasks
+// ------------------
+
+// Optimizing CSS and JavaScript
+gulp.task('useref', function() {
+
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'));
+});
+
+// Optimizing Images
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+      interlaced: true,
+    })))
+    .pipe(gulp.dest('dist/images'))
+})
+
+// Copying fonts
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+// Cleaning
+gulp.task('clean', function() {
+  return del.sync('dist').then(function(cb) {
+    return cache.clearAll(cb);
+  });
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
+
+// Build Sequences
+// -----------------
+
+gulp.task('default', function(callback) {
+  runSequence(
+    'sass',
+    ['browserSync', 'watch'],
+    callback
+  )
+});
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist',
+    ['sass', 'images', 'fonts'],
+    'useref',
+    callback
+  )
 });
